284d2d97d889ec900b53e1a76d83b01ba00a97fd
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-24 13:41:48 +0200

    useState in cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import './Product.css';
 
-export default class Product extends Component {
-  render() {
-    return(
-      <div className="wrapper">
-        <div>
-          Shopping Cart: 0 total items.
-        </div>
-        <div>Total: 0</div>
-
-        <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-        <button>Add</button> <button>Remove</button>
+export default function Product() {
+  const [cart, setCart] = useState([]);
+  return(
+    <div className="wrapper">
+      <div>
+        Shopping Cart: {cart.length} total items.
       </div>
-    )
-  }
+      <div>Total: 0</div>
+
+      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+      <button>Add</button> <button>Remove</button>
+    </div>
+  )
 }
