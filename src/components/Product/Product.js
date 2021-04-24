6ec89c5d51446309a79d0c4c94930df0ff3a71d5
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-24 13:57:29 +0200

    move function outside of component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+ nano src/components/Product/Product.js1~ nano src/components/Product/Product.js1~import React, { useState } from 'react';
 import './Product.css';
 
 const currencyOptions = {
@@ -6,20 +6,21 @@ const currencyOptions = {
   maximumFractionDigits: 2,
 }
 
+function getTotal(total) {
+  return total.toLocaleString(undefined, currencyOptions)
+}
+
 export default function Product() {
   const [cart, setCart] = useState([]);
-  const [total, setTotal] = useState(0); 
- 
- function getTotal() {
-    return total.toLocaleString(undefined, currencyOptions)
-  }
- 
+  const [total, setTotal] = useState(0);
+
+
   return(
     <div className="wrapper">
       <div>
         Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: {getTotal()}</div>
+      <div>Total: {getTotal(total)}</div><^>
 
       <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
       <button>Add</button> <button>Remove</button>
