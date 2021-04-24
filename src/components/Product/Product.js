fb74de53f959586abd945e3e853e3446865d5447
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-24 13:51:56 +0200

    convert total from a number to a string with two decimal places

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import './Product.css';
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
 export default function Product() {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0); 
+ 
+ function getTotal() {
+    return total.toLocaleString(undefined, currencyOptions)
+  }
+ 
   return(
     <div className="wrapper">
       <div>
         Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: {total}</div>
+      <div>Total: {getTotal()}</div>
 
       <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
       <button>Add</button> <button>Remove</button>
